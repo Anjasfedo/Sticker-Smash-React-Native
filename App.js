b51e7f4510dfad603fef71334bddc7c76b453a71
AnjasFedo <fedoaf98@gmail.com>
2024-01-09 20:34:11 +0700

    Save on Web Handler & Double Tap Gesture

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Platform } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import * as MediaLibrary from "expo-media-library";
 import { captureRef } from "react-native-view-shot";
+import domtoimage from "dom-to-image";
 
 const placeholderImage = require("./assets/images/background-image.png");
 
@@ -52,18 +53,34 @@ export default function App() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert("Saved!");
+    if (Platform.OS !== "web") {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement("a");
+        link.download = "sticker-smash.jpeg";
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
@@ -112,7 +129,7 @@ export default function App() {
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </GestureHandlerRootView>
   );
 }
--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,61 +1,78 @@
+import React from "react";
 import { View, Image } from "react-native";
-import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import {
+  GestureHandlerRootView,
+  PanGestureHandler,
+  TapGestureHandler,
+  State,
+} from "react-native-gesture-handler";
 import Animated, {
+  useAnimatedGestureHandler,
   useAnimatedStyle,
   useSharedValue,
   withSpring,
 } from "react-native-reanimated";
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
-  const scaleImage = useSharedValue(imageSize);
-
-  const doubleTap = Gesture.Tap()
-    .numberOfTaps(2)
-    .onStart(() => {
-      if (scaleImage.value !== imageSize * 2) {
-        scaleImage.value = scaleImage.value * 2;
-      }
-    });
-
-  const imageStyle = useAnimatedStyle(() => {
-    return {
-      width: withSpring(scaleImage.value),
-      height: withSpring(scaleImage.value),
-    };
-  });
-
+  const scaleImage = useSharedValue(1);
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
 
-  const drag = Gesture.Pan().onChange((event) => {
-    translateX.value += event.changeX;
-    translateY.value += event.changeY;
+  const panGestureHandler = useAnimatedGestureHandler({
+    onStart: (_, ctx) => {
+      ctx.startX = translateX.value;
+      ctx.startY = translateY.value;
+    },
+    onActive: (event, ctx) => {
+      translateX.value = ctx.startX + event.translationX;
+      translateY.value = ctx.startY + event.translationY;
+    },
+  });
+
+  const doubleTapGestureHandler = useAnimatedGestureHandler({
+    onActive: () => {
+      if (scaleImage.value !== 1) {
+        scaleImage.value = withSpring(1);
+      } else {
+        scaleImage.value = withSpring(2);
+      }
+    },
+  });
+
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(imageSize * scaleImage.value),
+      height: withSpring(imageSize * scaleImage.value),
+    };
   });
 
   const containerStyle = useAnimatedStyle(() => {
     return {
       transform: [
-        {
-          translateX: translateX.value,
-        },
-        {
-          translateY: translateY.value,
-        },
+        { translateX: translateX.value },
+        { translateY: translateY.value },
       ],
     };
   });
 
   return (
-    <Animated.View style={[containerStyle, { top: -350 }]}>
-      <GestureDetector gesture={drag}>
-        <GestureDetector gesture={doubleTap}>
-          <Animated.Image
-            source={stickerSource}
-            resizeMode="contain"
-            style={[imageStyle, { width: imageSize, height: imageSize }]}
-          />
-        </GestureDetector>
-      </GestureDetector>
-    </Animated.View>
+    <GestureHandlerRootView>
+      <PanGestureHandler onGestureEvent={panGestureHandler}>
+        <Animated.View style={[containerStyle]}>
+          <TapGestureHandler
+            onGestureEvent={doubleTapGestureHandler}
+            numberOfTaps={2}
+          >
+            <Animated.View>
+              <Animated.Image
+                source={stickerSource}
+                resizeMode="contain"
+                style={[imageStyle, { width: imageSize, height: imageSize }]}
+              />
+            </Animated.View>
+          </TapGestureHandler>
+        </Animated.View>
+      </PanGestureHandler>
+    </GestureHandlerRootView>
   );
 }
